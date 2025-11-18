c140ffbf9c64b1018bf9af7b6839078588371bce
reginaldo-sda <sda.reginaldo@gmail.com>
2025-11-17 22:08:33 -0300

    Atualização

---
+++ Comandos/1 poweroff.sh
@@ -0,0 +1,16 @@
+ 1  poweroff
+    2  sudo apt update
+    3  sudo -i
+    4  sudo apt
+    5* sudo apt upda
+    6  sudo apt upgrade
+    7  sudo apt dist-upgrade 
+    8  sudo apt full-upgrade
+    9  sudo apt autoremove
+   10  sudo apt autoclean
+   11  sudo apt clean
+   12  sudo apt update
+   13  sudo apt install git vim pip python
+   14  sudo apt install git vim pip python3
+   15  histori
+   16  history
